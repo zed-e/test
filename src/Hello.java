d62a6a1245d44f6b76b081dae4a268f74a66eb7a
zed-e <zhangcl_work@>
2019-09-18 11:09:59 +0800

    test

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i+"*"+j+"="+j*i+"	");
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
